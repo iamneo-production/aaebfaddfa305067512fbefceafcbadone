659c14ed378455b10107500b8ff2f8e0fe449deb
Iamneo <techadmin@iamneo.ai>
2023-09-13 12:41:42 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/example/springapp/controller/MedicineController.java
@@ -0,0 +1,33 @@
+package com.example.springapp.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springapp.model.Medicine;
+import com.example.springapp.service.MedicineService;
+
+@RestController
+public class MedicineController {
+    
+    
+        @Autowired
+        private MedicineService medicineService;
+    
+        @PostMapping
+        public boolean addMedicine(@RequestBody Medicine medicine) {
+            return medicineService.addMedicine(medicine);
+        }
+    
+        // @PutMapping("/{medicineId}")
+        // public Medicine updateMedicine(@PathVariable int medicineId, @RequestBody Medicine medicine) {
+        //     return medicineService.updateMedicine(medicineId, medicine);
+        // }
+    
+       
+    }
+    
---
+++ springapp/src/main/java/com/example/springapp/service/MedicineService.java
@@ -0,0 +1,38 @@
+package com.example.springapp.service;
+
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.example.springapp.model.Medicine;
+
+@Service
+public class MedicineService {
+
+    private final Map<Integer, Medicine> medicines = new HashMap<>();
+    private int nextMedicineId = 1;
+
+    public boolean addMedicine(Medicine medicine) {
+        medicine.setMedicineId(nextMedicineId++);
+        medicines.put(medicine.getMedicineId(), medicine);
+        return true;
+    }
+
+  
+
+    public Medicine updateMedicine(int medicineId, Medicine updatedMedicine) {
+        if (medicines.containsKey(medicineId)) {
+            Medicine existingMedicine = medicines.get(medicineId);
+            existingMedicine.setMedicineName(updatedMedicine.getMedicineName());
+            existingMedicine.setPrice(updatedMedicine.getPrice());
+            existingMedicine.setQuantity(updatedMedicine.getQuantity());
+            existingMedicine.setDescription(updatedMedicine.getDescription());
+            return existingMedicine;
+        }
+        return null;
+    }
+
+}
